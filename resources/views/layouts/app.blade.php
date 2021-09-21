070727fd83e2d7257219c7064db8b6c990b594a3
DANIEL QUIROZ <danykyroz@gmail.com>
2021-09-21 06:05:44 -0500

    fix: pruebas boostrap

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,56 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>Admin</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sticky-footer/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <style type="text/css">
+      /* Custom page CSS
+-------------------------------------------------- */
+/* Not required for template or sticky footer method. */
+
+.container {
+  width: auto;
+  max-width: 680px;
+  padding: 0 15px;
+}
+footer{
+  position: absolute;
+  bottom: 0;
+  text-align: center;
+}
+    </style>
+  </head>
+
+    <body class="d-flex flex-column h-100">
+    
+<!-- Begin page content -->
+<main class="flex-shrink-0">
+  <div class="container">
+       @yield('content')
+  </div>
+</main>
+
+<footer class="footer mt-auto py-3 bg-light">
+  <div class="container">
+    <span class="text-muted">Place sticky footer content here.</span>
+  </div>
+</footer>
+
+
+    
+  </body>
+</html>
+</html>
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,2 +1,9 @@
+ 
+@extends('layouts.app')
+
+@section('content')
+
 <a href="/twitter/mensajes/directos">Listar Ultimos Mensajes</a><br/>
 <a href="/twitter/perfil">Listar Perfil</a>
+
+@endsection
